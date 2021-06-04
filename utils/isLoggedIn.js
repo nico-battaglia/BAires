4d2ff27c4e53078fa98fa81d1192678e96771018
nico-battaglia <nico-battaglia@hotmail.com>
2021-06-04 03:10:06 +0000

    Add flash message

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -2,6 +2,7 @@ const isLoggedIn = (req, res, next)=>{
 	if(req.isAuthenticated()){
 		return next();
 	} else {
+		req.flash("error", "You must be logged in to do that");
 		res. redirect("/login");
 	}
 };
